8d894eaa65b4b8ec8a46cba1fd93b5d8f150ecba
Semyonenko Ivan <vsem2004@gmail.com>
2023-11-07 14:37:51 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ let sortBtnActive;
 
 let cards = [];
 
-fetch('../cards.json')
+fetch('https://whosemivan.github.io/diseasedproject/cards.json')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
